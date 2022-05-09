003d47d427a07cd9f03b7513d07488508c45744e
Ilya Malinowski <malinowski221106@gmail.com>
2022-05-09 22:18:59 +0300

    task 2 landmarks-BFS in progress

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,6 @@
 from collections import deque
+from typing import List, Dict
+import random
 
 
 def load_graph(path):
@@ -52,3 +54,57 @@ def find_components(graph):  # graph : Dictionary = { node : { node1, node2, nod
             components.append(path)
             used += path
     return components
+
+
+# to do nice to make command to illustrate work
+# bfs witch stores the parent in bfs tree
+def bfs(graph, start: int, log=False):  # graph : Dictionary = { node : { node1, node2, node3} }
+    if log:
+        print("bfs from {}".format(start))
+    queue, path = [], {}  # path : a pair of node : parent to start in tree
+    queue.append(start)
+    path[start] = start
+    while queue:
+        node = queue.pop(0)
+        for neighbour in graph[node]:
+            if neighbour not in path.keys():
+                queue.append(neighbour)
+                path[neighbour] = node
+    return path
+
+
+# ----------------------------------------LANDMARK-BFS----------------------------------------
+
+class Node:
+
+    def __init__(self, key):
+        self.key = key
+        self.parents: Dict[int, int] = {}  # dictionary of parents to landmarks
+
+    def parent_to(self, u, v):  # parent v to landmark u
+        self.parents[u] = v
+
+
+NODES: Dict[int, Node] = {}  # dic of pairs key:Node
+
+
+def precompute(graph: dict, log=False):
+    landmarks = [random.choice(list(graph.keys())) for _ in range(3)]
+    for u in landmarks:
+        parent_to_u = bfs(graph, u)
+        if log:
+            print("path to {} is {}".format(u, parent_to_u))
+        for node in parent_to_u.keys():
+            if node not in NODES.keys():
+                NODES[node] = Node(node)
+            NODES[node].parent_to(u, parent_to_u[node])
+
+
+def path_to_u(u: int, s: int):
+    path = [s]
+    if u not in NODES[s].parents:
+        raise ValueError('{} is not in U nodes'.format(u))
+    while s != u:
+        s = NODES[s].parents[u]
+        path.append(s)
+    return path
--- graph.py
+++ graph.py
@@ -13,14 +13,12 @@ print("Edges {}".format(edges))
 print("Density {}".format(edges / (nodes * (nodes - 1) / 2)))
 components = algorithms.find_components(graph)
 print("Components {}".format(len(components)))
+max_component = max(components, key=len)
 print("Max component {}% of all nodes".format(
-    len(max(components, key=len))/nodes * 100
+    len(max_component) / nodes * 100
 ))
+print("---A.2---")
+algorithms.precompute(graph)
+# print(algorithms.NODES[7].parents)
+print(algorithms.path_to_u(0, 5))
 print("---------")
-
-# print(graph)
-# path = algorithms.dfs(graph, min(graph.keys()))
-# cur = time.time() * 1000
-# path = algorithms.dfs(graph, 3)
-# print("time {}".format((time.time() * 1000 - cur)))
-# print(path)
